09831c73f3bd933dc894c7bdc511bce76edddf1c
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-05-30 21:17:16 -0700

    resizing now works

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ use vulkano::swapchain::{SurfaceTransform, Swapchain, PresentMode};
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::buffer::BufferUsage;
 use vulkano::swapchain;
+use vulkano::swapchain::{SwapchainCreationError, AcquireError};
 use vulkano::command_buffer::AutoCommandBufferBuilder;
 use vulkano::command_buffer::DynamicState;
 use vulkano::pipeline::viewport::Viewport;
 use vulkano::sync;
-use vulkano::sync::GpuFuture;
+use vulkano::sync::{GpuFuture, FlushError};
 use vulkano_win::VkSurfaceBuild;
 use winit::Event;
 use winit::EventsLoop;
@@ -90,30 +91,24 @@ fn main() {
 
     let queue = queues.next().unwrap();
 
-    // Step 5: Create vertex buffer
-    vulkano::impl_vertex!(Vertex, position);
-
-    let vertex1 = Vertex { position: [-0.9,  0.0] };
-    let vertex2 = Vertex { position: [ 0.9,  0.0] };
-    let vertex3 = Vertex { position: [ 0.0, -0.9] };
-
-    let vertex4 = Vertex { position: [ 0.9,  0.0] };
-    let vertex5 = Vertex { position: [ 0.0,  0.9] };
-    let vertex6 = Vertex { position: [-0.9,  0.0] };
-
-    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), 
-                                                       BufferUsage::vertex_buffer(),
-                                                       vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6].into_iter()
-                                                       ).unwrap();
-
 
     // Step 6: Create windows with event loop
     let mut events_loop = EventsLoop::new();
     let surface = WindowBuilder::new().build_vk_surface(&events_loop, instance.clone()).unwrap();
+    let window = surface.window();
 
     // Step 7: get the capabilities of the surface
     let caps = surface.capabilities(physical).unwrap();
-    let dimensions = caps.current_extent.unwrap_or([1280, 1024]);
+
+    let dimensions = if let Some(dimensions) = window.get_inner_size() {
+        // convert to physical pixels
+        let dimensions: (u32, u32) = dimensions.to_physical(window.get_hidpi_factor()).into();
+        [dimensions.0, dimensions.1]
+    } else {
+        // The window no longer exists so exit the application.
+        panic!("Unable to get dimension");
+    };
+
     let alpha = caps.supported_composite_alpha.iter().next().unwrap();
     let format = caps.supported_formats[0].0;
 
@@ -123,10 +118,6 @@ fn main() {
                                              caps.min_image_count, format, dimensions, 1, caps.supported_usage_flags, &queue, 
                                              SurfaceTransform::Identity, alpha, PresentMode::Fifo, true, None).unwrap();
 
-
-    // Step 9: Load the shader
-    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module.");
-    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module.");
 
     // Step 10: Setup render pass
     let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(), 
@@ -144,20 +135,9 @@ fn main() {
         }).unwrap());
 
 
-    // Step 11: Setup graphics pipeline
-    let pipeline = Arc::new(GraphicsPipeline::start()
-                            .vertex_input_single_buffer::<Vertex>()
-                            .vertex_shader(vs.main_entry_point(), ())
-                            //.triangle_list()
-                            .viewports_dynamic_scissors_irrelevant(1)
-                            .fragment_shader(fs.main_entry_point(), ())
-                            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
-                            .build(device.clone())
-                            .unwrap()
-                            );
 
     // Step 12: Create Dynamic State
-    let dynamic_state = {
+    let mut dynamic_state = {
         let dims = images[0].dimensions();
         let viewport =  Viewport {
             origin: [0.0, 0.0],
@@ -170,13 +150,48 @@ fn main() {
     };
 
     // Step 13: Create Frame buffers from dynamic state, render passes, and swapchain images
-    let framebuffers = images.iter().map(|image| {
+    let mut framebuffers = images.iter().map(|image| {
         Arc::new(
             Framebuffer::start(render_pass.clone())
             .add(image.clone()).unwrap()
             .build().unwrap()) as Arc<FramebufferAbstract + Send + Sync>
     }).collect::<Vec<_>>();
+    
+    // Step 5: Create vertex buffer
+    vulkano::impl_vertex!(Vertex, position);
 
+    let vertex1 = Vertex { position: [-0.9,  0.0] };
+    let vertex2 = Vertex { position: [ 0.9,  0.0] };
+    let vertex3 = Vertex { position: [ 0.0, -0.9] };
+
+    let vertex4 = Vertex { position: [ 0.9,  0.0] };
+    let vertex5 = Vertex { position: [ 0.0,  0.9] };
+    let vertex6 = Vertex { position: [-0.9,  0.0] };
+
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), 
+                                                       BufferUsage::vertex_buffer(),
+                                                       vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6].into_iter()
+                                                       ).unwrap();
+
+    // Step 9: Load the shader
+    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module.");
+    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module.");
+
+
+    // Step 11: Setup graphics pipeline
+    let pipeline = Arc::new(GraphicsPipeline::start()
+                            .vertex_input_single_buffer::<Vertex>()
+                            .vertex_shader(vs.main_entry_point(), ())
+                            //.triangle_list()
+                            .viewports_dynamic_scissors_irrelevant(1)
+                            .fragment_shader(fs.main_entry_point(), ())
+                            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+                            .build(device.clone())
+                            .unwrap()
+                            );
+
+
+    let mut recreate_swapchain = false;
 
     // Get a GPU Future pointer?
     let mut previous_frame_end = Box::new(sync::now(device.clone())) as Box<GpuFuture>;
@@ -185,8 +200,62 @@ fn main() {
         // clean up previous frame.
         previous_frame_end.cleanup_finished();
 
+        if recreate_swapchain {
+            let dimensions = if let Some(dimensions) = window.get_inner_size() {
+                // convert to physical pixels
+                let dimensions: (u32, u32) = dimensions.to_physical(window.get_hidpi_factor()).into();
+                [dimensions.0, dimensions.1]
+            } else {
+                // The window no longer exists so exit the application.
+                return;
+            };
+
+            // Step 8: Create a swapchain
+            // TODO: Swapchain is mutatable because we need to recreate it on resize.
+            let (new_swapchain, new_images) = match swapchain.recreate_with_dimension(dimensions) {
+                Ok(r) => r, 
+                // The user is in the process of resizing or smth. Just keep going. What could possibly go wrong?!
+                Err(SwapchainCreationError::UnsupportedDimensions) => continue,
+                Err(err) => panic!("{:?}", err),
+            };
+
+            swapchain = new_swapchain;
+
+
+            // Step 12: Create Dynamic State
+            dynamic_state = {
+                let dims = new_images[0].dimensions();
+                let viewport =  Viewport {
+                    origin: [0.0, 0.0],
+                    dimensions: [dims[0] as f32, dims[1] as f32],
+                    depth_range: 0.0 .. 1.0
+                };
+                DynamicState {
+                    viewports: Some(vec![viewport]), .. DynamicState::none()
+                }
+            };
+
+            // Step 13: Create Frame buffers from dynamic state, render passes, and swapchain images
+            framebuffers = new_images.iter().map(|image| {
+                Arc::new(
+                    Framebuffer::start(render_pass.clone())
+                    .add(image.clone()).unwrap()
+                    .build().unwrap()) as Arc<FramebufferAbstract + Send + Sync>
+            }).collect::<Vec<_>>();
+
+            recreate_swapchain = false;
+
+        }
+
         // Get the next image in the swapchain.
-        let (image_num, acquire_future) = swapchain::acquire_next_image(swapchain.clone(), None).unwrap();
+        let (image_num, acquire_future) = match swapchain::acquire_next_image(swapchain.clone(), None) {
+            Ok(r) => r,
+            Err(AcquireError::OutOfDate) => {
+                recreate_swapchain = true;
+                continue;
+            }
+            Err(err) => panic!("{:?}", err)
+        };
 
         let clear_values = vec!([0.0, 0.0, 0.5, 1.0].into());
 
@@ -209,10 +278,10 @@ fn main() {
             Ok(future) => {
                 previous_frame_end = Box::new(future) as Box<_>;
             }
-            //Err(FlushError::OutOfDate) => {
-                //recreate_swapchain = true;
-                //previous_frame_end = Box::new(sync::now(device.clone())) as Box<_>;
-            //}
+            Err(FlushError::OutOfDate) => {
+                recreate_swapchain = true;
+                previous_frame_end = Box::new(sync::now(device.clone())) as Box<_>;
+            }
             Err(e) => {
                 println!("{:?}", e);
                 previous_frame_end = Box::new(sync::now(device.clone())) as Box<_>;
@@ -224,7 +293,7 @@ fn main() {
         events_loop.poll_events(|event| {
             match event {
                 Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => done = true,
-                //Event::WindowEvent { event: WindowEvent::Resized(_), .. } => recreate_swapchain = true,
+                Event::WindowEvent { event: WindowEvent::Resized(_), .. } => recreate_swapchain = true,
                 _ => ()
             }
         });
