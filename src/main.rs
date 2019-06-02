9b7072a1a5a9f7a4fff353f64f8266e4f135ebfd
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2019-06-02 10:14:38 -0700

    Moved init code into VulkanInit class. Also added cgmath.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use vulkano::instance::Instance;
 use vulkano::instance::PhysicalDevice;
 use vulkano::device::Device;
+use vulkano::device::Queue;
 use vulkano::device::DeviceExtensions;
 use vulkano::device::Features;
 use vulkano::pipeline::GraphicsPipeline;
@@ -28,38 +29,60 @@ struct Vertex {
     position: [f32; 2], 
 }
 
+struct VulkanInit<'a> {
+    physical: PhysicalDevice<'a>,
+    device: Arc<Device>,
+    queue: Arc<Queue>
+}
+
+impl<'a> VulkanInit<'a> {
+    fn create(instance: &'a Arc<Instance>) -> VulkanInit<'a> {
+        // Step 2: Find physical device
+        // The iterator has the same lifetime as the instance.
+        let physical = PhysicalDevice::enumerate(&instance).next().expect("No physical devices available.");
+
+
+        // Step 3: Find queue families
+        for family in physical.queue_families() {
+            println!("Found a queue family with {:?} queues", family.queues_count());
+        }
+
+        let queue_family = physical.queue_families()
+            .find(|&q| q.supports_graphics())
+            .expect("Couldn't find a graphical queue family.");
+
+        // Step 4: Create Device and queue. 
+        let (device, mut queues) = {
+            let device_ext = DeviceExtensions  {
+                khr_swapchain: true,
+                .. DeviceExtensions::none()
+            };
+            Device::new(physical, &Features::none(), &device_ext, 
+                        [(queue_family, 0.5)].iter().cloned())
+                .expect("Failed to create device.")
+        };
+
+        let queue = queues.next().unwrap();
+
+        VulkanInit { 
+            physical: physical,
+            device: device,
+            queue: queue
+        }
+    }
+}
+
+fn create_instance() -> Arc<Instance> {
+    let extentions = vulkano_win::required_extensions();
+    Instance::new(None, &extentions, None) .expect("Failed to create instance.")
+}
 
 fn main() {
+
     // Step 1: Create Instance
-    let instance = {
-        let extentions = vulkano_win::required_extensions();
-        Instance::new(None, &extentions, None) .expect("Failed to create instance.")
-    };
+    let instance = create_instance();
+    let initializer = VulkanInit::create(&instance);
 
-    // Step 2: Find physical device
-    let physical = PhysicalDevice::enumerate(&instance).next().expect("No physical devices available.");
-
-    // Step 3: Find queue families
-    for family in physical.queue_families() {
-        println!("Found a queue family with {:?} queues", family.queues_count());
-    }
-
-    let queue_family =   physical.queue_families()
-        .find(|&q| q.supports_graphics())
-        .expect("Couldn't find a graphical queue family.");
-
-    // Step 4: Create Device and queue. 
-    let (device, mut queues) = {
-        let device_ext = DeviceExtensions  {
-            khr_swapchain: true,
-            .. DeviceExtensions::none()
-        };
-        Device::new(physical, &Features::none(), &device_ext, 
-                    [(queue_family, 0.5)].iter().cloned())
-            .expect("Failed to create device.")
-    };
-
-    let queue = queues.next().unwrap();
 
 
     // Step 6: Create windows with event loop
@@ -68,7 +91,7 @@ fn main() {
     let window = surface.window();
 
     // Step 7: get the capabilities of the surface
-    let caps = surface.capabilities(physical).unwrap();
+    let caps = surface.capabilities(initializer.physical).unwrap();
 
     let dimensions = if let Some(dimensions) = window.get_inner_size() {
         // convert to physical pixels
@@ -83,14 +106,13 @@ fn main() {
     let format = caps.supported_formats[0].0;
 
     // Step 8: Create a swapchain
-    // TODO: Swapchain is mutatable because we need to recreate it on resize.
-    let (mut swapchain, images) = Swapchain::new(device.clone(), surface.clone(),
-                                             caps.min_image_count, format, dimensions, 1, caps.supported_usage_flags, &queue, 
+    let (mut swapchain, images) = Swapchain::new(initializer.device.clone(), surface.clone(),
+                                             caps.min_image_count, format, dimensions, 1, caps.supported_usage_flags, &initializer.queue, 
                                              SurfaceTransform::Identity, alpha, PresentMode::Fifo, true, None).unwrap();
 
 
     // Step 10: Setup render pass
-    let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(), 
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(initializer.device.clone(), 
         attachments: {
             color: {
                 load: Clear,
@@ -136,12 +158,12 @@ fn main() {
 
     let vertex4 = Vertex { position: [ 0.0,  0.9] };
 
-    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), 
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(initializer.device.clone(), 
                                                        BufferUsage::vertex_buffer(),
                                                        vec![vertex1, vertex2, vertex3, vertex4].into_iter()
                                                        ).unwrap();
 
-    let index_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::index_buffer(),
+    let index_buffer = CpuAccessibleBuffer::from_iter(initializer.device.clone(), BufferUsage::index_buffer(),
                                                       vec![0, 1, 2, 0, 3, 1].into_iter().map(|x| x as u32)).unwrap();
 
 
@@ -153,8 +175,11 @@ fn main() {
     #version 450
 
     layout(location = 0) in vec2 position;
+    layout(location = 0) out vec3 v_color;
+
 
     void main() {
+        v_color = vec3(0.5*position + 0.5, 0.5);
         gl_Position = vec4(position, 0.0, 1.0);
     }
     "
@@ -167,18 +192,19 @@ fn main() {
             ty: "fragment",
             src: "
     #version 450
+    layout(location = 0) in vec3 v_color;
     layout(location = 0) out vec4 f_color;
 
     void main() {
-        f_color = vec4(0.5, 0.0, 0.0, 1.0);
+        f_color = vec4(v_color, 1.0);
     }
     "
         }
     }
 
     // Step 9: Load the shader
-    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module.");
-    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module.");
+    let vs = vs::Shader::load(initializer.device.clone()).expect("failed to create shader module.");
+    let fs = fs::Shader::load(initializer.device.clone()).expect("failed to create shader module.");
 
 
     // Step 11: Setup graphics pipeline
@@ -189,7 +215,7 @@ fn main() {
                             .viewports_dynamic_scissors_irrelevant(1)
                             .fragment_shader(fs.main_entry_point(), ())
                             .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
-                            .build(device.clone())
+                            .build(initializer.device.clone())
                             .unwrap()
                             );
 
@@ -197,7 +223,7 @@ fn main() {
     let mut recreate_swapchain = false;
 
     // Get a GPU Future pointer?
-    let mut previous_frame_end = Box::new(sync::now(device.clone())) as Box<GpuFuture>;
+    let mut previous_frame_end = Box::new(sync::now(initializer.device.clone())) as Box<GpuFuture>;
 
     loop {
         // clean up previous frame.
@@ -214,7 +240,6 @@ fn main() {
             };
 
             // Step 8: Create a swapchain
-            // TODO: Swapchain is mutatable because we need to recreate it on resize.
             let (new_swapchain, new_images) = match swapchain.recreate_with_dimension(dimensions) {
                 Ok(r) => r, 
                 // The user is in the process of resizing or smth. Just keep going. What could possibly go wrong?!
@@ -260,10 +285,10 @@ fn main() {
             Err(err) => panic!("{:?}", err)
         };
 
-        let clear_values = vec!([0.0, 0.0, 0.5, 1.0].into());
+        let clear_values = vec!([0.02, 0.02, 0.02, 1.0].into());
 
         // Create the command buffer for this frame
-        let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap()
+        let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(initializer.device.clone(), initializer.queue.family()).unwrap()
             .begin_render_pass(framebuffers[image_num].clone(), false, clear_values).unwrap()
             .draw_indexed(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), index_buffer.clone(), (), ()).unwrap()
             .end_render_pass().unwrap()
@@ -271,8 +296,8 @@ fn main() {
 
         // Execute the commands in the command buffer, Present the image in the swapchain
         let future = previous_frame_end.join(acquire_future)
-            .then_execute(queue.clone(), command_buffer).unwrap()
-            .then_swapchain_present(queue.clone(), swapchain.clone(), image_num)
+            .then_execute(initializer.queue.clone(), command_buffer).unwrap()
+            .then_swapchain_present(initializer.queue.clone(), swapchain.clone(), image_num)
             .then_signal_fence_and_flush();
 
         // If it worked, the previous frame is now this frame. Otherwise, log the error and set the
@@ -283,11 +308,11 @@ fn main() {
             }
             Err(FlushError::OutOfDate) => {
                 recreate_swapchain = true;
-                previous_frame_end = Box::new(sync::now(device.clone())) as Box<_>;
+                previous_frame_end = Box::new(sync::now(initializer.device.clone())) as Box<_>;
             }
             Err(e) => {
                 println!("{:?}", e);
-                previous_frame_end = Box::new(sync::now(device.clone())) as Box<_>;
+                previous_frame_end = Box::new(sync::now(initializer.device.clone())) as Box<_>;
             }
         }
 
